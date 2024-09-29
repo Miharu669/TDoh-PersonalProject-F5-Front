ac3306334bcd300eecf89df43ec96966c89e5971
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-29 20:33:49 +0200

    footer done

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { RouterView, useRoute } from "vue-router";
 import { computed } from "vue";
 import Navbar from "@/components/Navbar/Navbar.vue";
+import Footer from "@/components/Footer/Footer.vue";
 
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -24,7 +24,7 @@ const isAuthenticated = computed(() => store.user.isAuthenticated);
 </script>
 
 <template>
-  <nav class="bg-primary p-4">
+  <nav class="bg-gray-900 p-4">
     <div class="container mx-auto flex items-center justify-between">
       <div class="text-white text-xl font-bold">
         <a href="/" class=" text-quinary text-2xl">TDoH</a>
